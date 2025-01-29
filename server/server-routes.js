e8689510a1c14e33d7848ba6677bf0a42d74fa2f
Nicholas <naw7961@rit.edu>
2025-01-29 11:14:23 -0700

    Team basic features complete
    finished CRUD for teams table

--- server/server-routes.js
+++ server/server-routes.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 const todos = require('./database/todo-queries.js');
 const users = require('./database/user-queries.js');
+const teams = require('./database/teams-queries.js');
 
 /// Create functions
 function createToDo(req, data) {
@@ -30,6 +31,17 @@ function createUser(req, data){
   }
 }
 
+function createTeam(req, data){
+  const protocol = req.protocol, 
+  host = req.get('host'), 
+  id = data.id;
+
+  return{
+    name: data.name,
+    url: `${protocol}://${host}/team/${id}`
+  }
+}
+
 /// Gets
 async function getAllTodos(req, res) {
   const allEntries = await todos.all();
@@ -41,6 +53,11 @@ async function getAllUsers(req, res){
   return res.send(allEntries.map( _.curry(createUser)(req) ));
 }
 
+async function getAllTeams(req, res){
+  const allEntries = await teams.all();
+  return res.send(allEntries.map( _.curry(createTeam)(req) ));
+}
+
 async function getTodo(req, res) {
   const todo = await todos.get(req.params.id);
   return res.send(todo);
@@ -49,6 +66,11 @@ async function getTodo(req, res) {
 async function getUser(req, res) {
   const user = await users.get(req.params.id);
   return res.send(user);
+}
+
+async function getTeam(req, res) {
+  const team = await teams.get(req.params.id);
+  return res.send(team);
 }
 
 /// Posts
@@ -67,6 +89,11 @@ async function postUser(req, res) {
   return res.send(createUser(req, created));
 }
 
+async function postTeam(req, res) {
+  const created = await teams.create(req.body.name);
+  return res.send(createTeam(req, created));
+}
+
 /// Patches
 async function patchTodo(req, res) {
   const patched = await todos.update(req.params.id, req.body);
@@ -76,6 +103,11 @@ async function patchTodo(req, res) {
 async function patchUser(req, res) {
   const patched = await users.update(req.params.id, req.body);
   return res.send(createUser(req, patched));
+}
+
+async function patchTeam(req, res) {
+  const patched = await teams.update(req.params.id, req.body);
+  return res.send(createTeam(req, patched));
 }
 
 /// Deletes
@@ -89,6 +121,11 @@ async function deleteAllUsers(req, res) {
   return res.send(deletedEntries.map( _.curry(createUser)(req) ));
 }
 
+async function deleteAllTeams(req, res) {
+  const deletedEntries = await teams.clear();
+  return res.send(deletedEntries.map( _.curry(createTeam)(req) ));
+}
+
 async function deleteTodo(req, res) {
   const deleted = await todos.delete(req.params.id);
   return res.send(createToDo(req, deleted));
@@ -97,6 +134,11 @@ async function deleteTodo(req, res) {
 async function deleteUser(req, res) {
   const deleted = await users.delete(req.params.id);
   return res.send(createUser(req, deleted));
+}
+
+async function deleteTeam(req, res) {
+  const deleted = await teams.delete(req.params.id);
+  return res.send(createTeam(req, deleted));
 }
 
 /// Errors
@@ -116,16 +158,22 @@ function addErrorReporting(func, message) {
 const toExport = {
     getAllTodos: { method: getAllTodos, errorMessage: "Could not fetch all todos" },
     getAllUsers: { method: getAllUsers, errorMessage: "Could not fetch all users" },
+    getAllTeams: { method: getAllTeams, errorMessage: "Could not fetch all teams" },
     getTodo: { method: getTodo, errorMessage: "Could not fetch todo" },
     getUser: { method: getUser, errorMessage: "Could not fetch user" },
+    getTeam: { method: getTeam, errorMessage: "Could not fetch team" },
     postTodo: { method: postTodo, errorMessage: "Could not post todo" },
     postUser: { method: postUser, errorMessage: "Could not post user" },
+    postTeam: { method: postTeam, errorMessage: "Could not post team" },
     patchTodo: { method: patchTodo, errorMessage: "Could not patch todo" },
     patchUser: { method: patchUser, errorMessage: "Could not patch user" },
+    patchTeam: { method: patchTeam, errorMessage: "Could not patch team" },
     deleteAllTodos: { method: deleteAllTodos, errorMessage: "Could not delete all todos" },
     deleteAllUsers: { method: deleteAllUsers, errorMessage: "Could not delete all users" },
+    deleteAllTeams: { method: deleteAllTeams, errorMessage: "Could not delete all teams" },
     deleteTodo: { method: deleteTodo, errorMessage: "Could not delete todo" },
-    deleteUser: { method: deleteUser, errorMessage: "Could not delete user" }
+    deleteUser: { method: deleteUser, errorMessage: "Could not delete user" },
+    deleteTeam: { method: deleteTeam, errorMessage: "Could not delete team" },
 }
 
 for (let route in toExport) {
--- server/server.js
+++ server/server.js
@@ -6,22 +6,28 @@ const port = process.env.PORT || 5000;
 /// gets
 app.get('/', routes.getAllTodos);
 app.get('/user', routes.getAllUsers);
+app.get('/team', routes.getAllTeams);
 app.get('/:id', routes.getTodo);
 app.get('/user/:id', routes.getUser);
+app.get('/team/:id', routes.getTeam);
 
 /// posts
 app.post('/', routes.postTodo);
 app.post('/user', routes.postUser);
+app.post('/team', routes.postTeam);
 
 /// patches
 app.patch('/:id', routes.patchTodo);
 app.patch('/user/:id', routes.patchUser);
+app.patch('/team/:id', routes.patchTeam);
 
 /// deletes
 app.delete('/', routes.deleteAllTodos);
 app.delete('/user', routes.deleteAllUsers);
+app.delete('/team', routes.deleteAllTeams);
 app.delete('/:id', routes.deleteTodo);
 app.delete('/user/:id', routes.deleteUser);
+app.delete('/team/:id', routes.deleteTeam);
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => console.log(`Listening on port ${port}`));
